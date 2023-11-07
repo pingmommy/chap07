2f5ab6ab616cd6ee08f23b39266968778603899c
mcar8764 <mcar8764@gmail.com>
2023-11-07 16:21:41 +0900

    ..

---
+++ src/com/example/TimerE.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import util.Alpha;
+
+public class TimerE {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
+
+
+
+//Timer t = new Timer();
+//
+//TimerTask tt = new TimerTask() {
+//
+//	@Override
+//	public void run() {
+//		Alpha a = new Alpha();
+//		a.show();
+//	}
+//	
+//};
+//
+//t.schedule(tt, 3000, 1000);
